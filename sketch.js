3942263e3d39e37eb55b6439b95855bc051e7c4f
Hade <mhade@MacBook-Pro.local>
2019-02-28 12:14:32 -0800

    randomer

--- sketch.js
+++ sketch.js
@@ -4,14 +4,12 @@ const random = require('canvas-sketch-util/random');
 const palettes = require('nice-color-palettes');
 
 const settings = {
-  // dimensions: [ 2040, 2040 ],
 };
 
 const sketch = () => {
-  // const palette = random.shuffle(random.pick(palettes));
   const palette = random.pick(palettes);
-  const featureColors = palette.slice(0,4);
-  const backgroundColor = palette.slice(4,5);
+  const backgroundColor = random.pick(palette);
+  palette.splice(palette.indexOf(backgroundColor),1);
 
   const createGrid = () => {
     const points = [];
@@ -22,7 +20,7 @@ const sketch = () => {
         const v = count <= 1 ? 0.2 : y/(count - 1);
         const radius = Math.abs(random.noise2D(u, v)) * 0.2;
         points.push({
-          color: random.pick(featureColors),
+          color: random.pick(palette),
           radius,
           rotation: random.noise2D(u, v),
           position: [u,v]
@@ -51,13 +49,7 @@ const sketch = () => {
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
       
-      // context.beginPath();
-      // context.arc(x, y, radius * width, 0, Math.PI * 2, false);
-      // context.strokeStyle = 'black';
-      // context.lineWidth = 20;
-      // context.fillStyle = color;
-      // context.fill();
-
+  
       context.save();
       context.fillStyle = color;
       context.font = `${radius * width}px "Helvetica"`;
